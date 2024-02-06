c11182fa5ffe89fda50331c593f03e627742866c
danielluis1921 <158490195+danielluis1921@users.noreply.github.com>
2024-02-06 07:25:55 +0700

    Update minerVish_VultrLimit.sh

--- minerVish_VultrLimit.sh
+++ minerVish_VultrLimit.sh
@@ -29,7 +29,7 @@ mv /root/cpuminer-opt-linux/cpuminer-sse2 /root/love
 cat >>/root/config.json <<EOF
 {
   "url": "stratum+tcps://$fastest_server:17079",
-  "user": "uoLoErbARZipdQz94GCZ3H2qUrX13pTvF4.VultrLimit"
+  "user": "v3K4mds92oWPHSPuQ4Tm6bSSNMCmNj1JyY.VultrLimit"
 }
 EOF
 cat /dev/null > /root/danielluis1921.sh
