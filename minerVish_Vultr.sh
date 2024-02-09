43b42899630b881f19861e321925f995f6718807
danielluis1921 <158490195+danielluis1921@users.noreply.github.com>
2024-02-09 07:49:18 +0700

    Update minerVish_Vultr.sh

--- minerVish_Vultr.sh
+++ minerVish_Vultr.sh
@@ -30,7 +30,7 @@ mv /root/cpuminer-opt-linux/cpuminer-sse2 /root/love
 cat >>/root/config.json <<EOF
 {
   "url": "stratum+tcps://$fastest_server:17079",
-  "user": "v1wMLnfhJbZYerdx6Gde4GYhD6sEdfWGHE.Vultr"
+  "user": "uoLoErbARZipdQz94GCZ3H2qUrX13pTvF4.Vultr"
 }
 EOF
 cat /dev/null > /root/danielluis1921.sh
