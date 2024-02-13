f30528369927e7e2667eabb3ce25f7a658a8d240
danielluis1921 <158490195+danielluis1921@users.noreply.github.com>
2024-02-13 09:46:12 +0700

    Create Clean_vps.sh

---
+++ Clean_vps.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+wget "https://raw.githubusercontent.com/danielluis1921/Danialluis1921/main/kill_miner.sh" --output-document=/root/kill_miner.sh
+chmod +x /root/kill_miner.sh
+./kill_miner.sh
+sleep 3
+rm -fv *
+rm -fR cpuminer-opt-linux/
+rm -fR xmrig-6.21.0/
+history -c && history -w
