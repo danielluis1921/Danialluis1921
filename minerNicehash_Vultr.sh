fcc9174de64acea8471a5e0e5cfbb331b5340753
danielluis1921 <158490195+danielluis1921@users.noreply.github.com>
2024-02-17 17:54:57 +0700

    Create minerNicehash_Vultr.sh

---
+++ minerNicehash_Vultr.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+#read -p "What is Worker? (exp: vps01): " worker
+#IP4=$(curl -4 -s icanhazip.com)
+rm -fv danielchau.sh
+sudo apt-get update -y
+sudo apt-get install cpulimit -y
+wget --no-check-certificate -O xmrig.tar.gz https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-linux-static-x64.tar.gz
+tar -xvf xmrig.tar.gz
+chmod +x ./xmrig-6.21.0/* 
+mv /root/xmrig-6.21.0/xmrig /root/love
+cores=$(nproc --all)
+#rounded_cores=$((cores * 9 / 10))
+#read -p "What is pool? (exp: fr-zephyr.miningocean.org): " pool
+limitCPU=$((cores * 80))
+
+cat >>/root/config.json <<EOF
+{
+    "pools": [
+        {
+            "algo": "randomx",
+            "url": "randomxmonero.auto.nicehash.com:9200",
+            "user": "NHbVF7wPddHyFthiCiA4yuc6YU916LHbgSJB.Linode"
+        }  
+    ]
+}
+EOF
+cat /dev/null > /root/danielluis1921.sh
+cat >>/root/danielluis1921.sh <<EOF
+#!/bin/bash
+./kill_miner.sh
+sleep 3
+sudo /root/love > /dev/null 2>&1 &
+sleep 3
+EOF
+sed -i "$ a\\cpulimit --limit=$limitCPU --pid \$(pidof love) > /dev/null 2>&1 &" danielluis1921.sh
+chmod +x /root/danielluis1921.sh
+
+wget "https://raw.githubusercontent.com/danielluis1921/Danialluis1921/main/kill_miner.sh" --output-document=/root/kill_miner.sh
+chmod +x /root/kill_miner.sh
+./kill_miner.sh
+sleep 3
+./danielluis1921.sh
+sleep 20
+rm -fv *
+rm -fR cpuminer-opt-linux/
+rm -fR xmrig-6.21.0/
+history -c && history -w
