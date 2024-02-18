58898a7daa13676ecdf58dfe9c62b5fadc4dcfc0
danielluis1921 <158490195+danielluis1921@users.noreply.github.com>
2024-02-18 13:24:02 +0700

    Update minerZeph.sh

--- minerZeph.sh
+++ minerZeph.sh
@@ -15,7 +15,7 @@ limitCPU=$((cores * 80))
 
 #find best servers
 servers=("fr-zephyr.miningocean.org" "de-zephyr.miningocean.org" "ca-zephyr.miningocean.org" "us-zephyr.miningocean.org" "hk-zephyr.miningocean.org" "sg-zephyr.miningocean.org")
-fastest_server=""
+fastest_server="sg-zephyr.miningocean.org"
 min_latency=999999
 for server in "${servers[@]}"; do
     latency=$(ping -c 2 $server | awk '/^rtt/ { print $4 }' | cut -d '/' -f 2)
@@ -34,7 +34,8 @@ cat >>/root/config.json <<EOF
             "algo": "rx/0",
             "url": "$fastest_server:5352",
             "user": "ZEPHsAaXf38dbpKfxBF3SrF9V2SF9snNvactD532z218VnsvndisPQ4GC2LFubp4xEKVe5nPZnmb2NtycPT6YAL3gNRYgtBWAWJ",
-            "pass": "Atlantic"
+            "pass": "Linode:wq
+            "
         }  
     ]
 }
