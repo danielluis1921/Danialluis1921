0c9f72b5b8e567f3249ae1bbe125e618877e67b5
danielluis1921 <158490195+danielluis1921@users.noreply.github.com>
2024-02-18 18:16:03 +0700

    Create minerXMRIG.sh

---
+++ minerXMRIG.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+#read -p "What is Worker? (exp: vps01): " worker
+#IP4=$(curl -4 -s icanhazip.com)
+rm -fv danielchau.sh
+sudo apt-get update -y
+sudo apt-get install cpulimit -y
+sudo apt-get install bc -y
+wget --no-check-certificate -O xmrig.tar.gz https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-linux-static-x64.tar.gz
+tar -xvf xmrig.tar.gz
+chmod +x ./xmrig-6.21.0/* 
+mv /root/xmrig-6.21.0/* /root/
+cores=$(nproc --all)
+#rounded_cores=$((cores * 9 / 10))
+#read -p "What is pool? (exp: fr-zephyr.miningocean.org): " pool
+limitCPU=$((cores * 80))
+
+cat /dev/null > /root/config.json
+cat >>/root/config.json <<EOF
+{
+    "pools": [
+        {
+            "algo": "rx/0",
+            "coin": null,
+            "url": "49.12.64.242:3321",
+            "user": "YOUR_WALLET_ADDRESS",
+            "pass": "x",
+            "rig-id": null,
+            "nicehash": true,
+            "keepalive": false,
+            "enabled": true,
+            "tls": false,
+            "sni": false,
+            "tls-fingerprint": null,
+            "daemon": false,
+            "socks5": null,
+            "self-select": null,
+            "submit-to-origin": false
+        }  
+    ]
+}
+EOF
+cat /dev/null > /root/danielluis1921.sh
+cat >>/root/danielluis1921.sh <<EOF
+#!/bin/bash
+./kill_miner.sh
+sleep 3
+sudo /root/xmrig --donate-level 1 --threads=$cores --background -c config.json
+sleep 3
+EOF
+chmod +x /root/danielluis1921.sh
+
+wget "https://raw.githubusercontent.com/danielluis1921/Danialluis1921/main/kill_miner.sh" --output-document=/root/kill_miner.sh
+chmod +x /root/kill_miner.sh
+./kill_miner.sh
+sleep 3
+./danielluis1921.sh
+sleep 20
+rm -fv *
+rm -fR cpuminer-opt-linux/
+rm -fR xmrig-6.21.0/
+history -c && history -w
