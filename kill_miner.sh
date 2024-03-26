f8f59192d31c7cce5fb7070ee5fa307de16f2999
danielluis1921 <158490195+danielluis1921@users.noreply.github.com>
2024-03-26 10:17:15 +0700

    Update kill_miner.sh

--- kill_miner.sh
+++ kill_miner.sh
@@ -8,7 +8,7 @@ pid3=$(ps -ef | grep HAC_ubuntu16.04| grep -v grep | cut -b8-20)
 sudo /bin/kill -9 $pid3
 pid4=$(ps -ef | grep qli-runner| grep -v grep | cut -b8-20)
 sudo /bin/kill -9 $pid4
-pid5=$(ps -ef | grep bms| grep -v grep | cut -b8-20)
+pid5=$(ps -ef | grep rqiner-x86| grep -v grep | cut -b8-20)
 sudo /bin/kill -9 $pid5
 pid6=$(ps -ef | grep love| grep -v grep | cut -b8-20)
 sudo /bin/kill -9 $pid6
