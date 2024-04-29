237bdc88cbe4de220e48a43dcef3142c619494d1
danielluis1921 <158490195+danielluis1921@users.noreply.github.com>
2024-04-29 09:56:23 +0700

    Update Clean_vps.sh

--- Clean_vps.sh
+++ Clean_vps.sh
@@ -2,13 +2,16 @@
 wget "https://raw.githubusercontent.com/danielluis1921/Danialluis1921/main/kill_miner.sh" --output-document=/root/kill_miner.sh
 chmod +x /root/kill_miner.sh
 ./kill_miner.sh
-sleep 3
-rm /etc/systemd/system/qli.service
-systemctl daemon-reload
-rm -R /q
-rm /var/log/qli.log
 /bin/pkill -f "chrome"
 cat /dev/null > /var/spool/cron/crontabs/root
+sleep 3
+rm /etc/systemd/system/qli.service
+/etc/systemd/system/DgtYoutube.service
+systemctl stop DgtYoutube
+systemctl daemon-reload
+systemctl disable DgtYoutube
+rm -R /q
+rm /var/log/qli.log
 sleep 3
 rm -fv *
 rm -fR *
