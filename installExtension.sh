09fc367d9dea4ef913413f481d4bbaf0da7264cb
danielluis1921 <158490195+danielluis1921@users.noreply.github.com>
2024-06-06 12:21:13 +0700

    Create installExtension.sh

---
+++ installExtension.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+wget https://github.com/danielluis1921/Danialluis1921/raw/main/ffejlioijcokmblckiijnjcmfidjppdn-2.1.0_0.crx
+wget https://github.com/danielluis1921/Danialluis1921/raw/main/lgmpfmgeabnnlemejacfljbmonaomfmm-2.1.0_0.crx
+mkdir extension
+mkdir extension/ffejlioijcokmblckiijnjcmfidjppdn
+mkdir extension/lgmpfmgeabnnlemejacfljbmonaomfmm
+unzip ffejlioijcokmblckiijnjcmfidjppdn-2.1.0_0.crx -d extension/ffejlioijcokmblckiijnjcmfidjppdn
+unzip lgmpfmgeabnnlemejacfljbmonaomfmm-2.1.0_0.crx -d extension/lgmpfmgeabnnlemejacfljbmonaomfmm
