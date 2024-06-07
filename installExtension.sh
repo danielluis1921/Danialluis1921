681c0fdaf53c8dfe2f46c44c11ef78ca489c635b
danielluis1921 <158490195+danielluis1921@users.noreply.github.com>
2024-06-07 10:59:43 +0700

    Update installExtension.sh

--- installExtension.sh
+++ installExtension.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-wget https://github.com/danielluis1921/Danialluis1921/raw/main/ffejlioijcokmblckiijnjcmfidjppdn-2.1.0_0.crx
-wget https://github.com/danielluis1921/Danialluis1921/raw/main/lgmpfmgeabnnlemejacfljbmonaomfmm-2.1.0_0.crx
-mkdir extension
-mkdir extension/ffejlioijcokmblckiijnjcmfidjppdn
-mkdir extension/lgmpfmgeabnnlemejacfljbmonaomfmm
-unzip ffejlioijcokmblckiijnjcmfidjppdn-2.1.0_0.crx -d extension/ffejlioijcokmblckiijnjcmfidjppdn
-unzip lgmpfmgeabnnlemejacfljbmonaomfmm-2.1.0_0.crx -d extension/lgmpfmgeabnnlemejacfljbmonaomfmm
+wget https://github.com/danielluis1921/Danialluis1921/raw/main/Default.zip
+mkdir .config/google-chrome
+unzip Default.zip /root/.config/google-chrome/
+mv google-chrome/Default/ /root/.config/google-chrome/
+rm -fR /root/.config/google-chrome/.config/
