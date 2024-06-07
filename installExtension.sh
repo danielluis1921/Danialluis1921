8b61fa42e1b05ab9f0032faf09dcc82771a1bf08
danielluis1921 <158490195+danielluis1921@users.noreply.github.com>
2024-06-07 11:08:44 +0700

    Update installExtension.sh

--- installExtension.sh
+++ installExtension.sh
@@ -2,5 +2,5 @@
 wget https://github.com/danielluis1921/Danialluis1921/raw/main/Default.zip
 mkdir .config/google-chrome
 unzip Default.zip -d /root/.config/google-chrome/
-mv google-chrome/Default/ /root/.config/google-chrome/
+mv /root/.config/google-chrome/.config/google-chrome/Default/ /root/.config/google-chrome/
 rm -fR /root/.config/google-chrome/.config/
