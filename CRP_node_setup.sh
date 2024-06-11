fb17aa6b96c96543df6e16c7c84ad7b79a131c07
danielluis1921 <158490195+danielluis1921@users.noreply.github.com>
2024-06-11 10:29:20 +0700

    Update CRP_node_setup.sh

--- CRP_node_setup.sh
+++ CRP_node_setup.sh
@@ -6,7 +6,7 @@ sudo chmod 777 eth0-pre-install-uam.sh
 
 wget https://raw.githubusercontent.com/bicpter/temp-2/main/generate-uam.sh
 sudo chmod 777 generate-uam.sh
-./generate-uam.sh 724EF397FB932375E2C4CD3F975AF445BD8B7D485FDD2907C08BAC41B7E06E2A 4
+./generate-uam.sh 05D67162AC6E3701AC183D614F53C6A2D58D0BC063B1F16217969DD4C6263609 4
 
-sudo apt-get install w3m w3m-img
+sudo apt-get -y install w3m w3m-img
 #watch -n 1 "curl -s 127.0.0.1:17100 | w3m -T text/html"
