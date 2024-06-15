025f809172ab573f85e11ecec45558d8782cbee6
danielluis1921 <158490195+danielluis1921@users.noreply.github.com>
2024-06-15 13:15:26 +0700

    Create Farcaster.sh

---
+++ Farcaster.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+sudo apt-get install screen -y
+
+wget https://download.thehubble.xyz/bootstrap.sh
+
+cat >>/root/Farcaster.exp <<EOF
+#!/usr/bin/expect -f
+set timeout -1
+spawn bash bootstrap.sh
+
+expect "> Enter your Ethereum Mainnet RPC URL: "
+send "https://mainnet.infura.io/v3/ae795a1c1453402ca616d1323ba5a819\r"
+
+expect "> Enter your Optimism L2 Mainnet RPC URL: "
+send "https://optimism-mainnet.infura.io/v3/ae795a1c1453402ca616d1323ba5a819\r"
+
+expect "> Your FID or farcaster username: "
+send "674637\r"
+
+
+# Wait for the script to complete
+expect eof
+EOF
+
+
+# Name of the screen session
+SCREEN_NAME="Hubble"
+
+# Path to the expect script
+EXPECT_SCRIPT="Farcaster.exp"
+
+# Start a new screen session and run the expect script
+screen -dmS $SCREEN_NAME expect $EXPECT_SCRIPT
+
+# Detach from the screen session (this is done automatically with -dmS option)
+#screen -S $SCREEN_NAME -X detach
+
+echo "Script is running in screen session: $SCREEN_NAME"
