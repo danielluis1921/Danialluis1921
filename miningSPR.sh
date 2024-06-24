9b41cc026d0000998439bbf30fd1260b62862a49
danielluis1921 <158490195+danielluis1921@users.noreply.github.com>
2024-06-24 19:29:09 +0700

    Update miningSPR.sh

--- miningSPR.sh
+++ miningSPR.sh
@@ -9,4 +9,4 @@ sudo apt-get update -y
 sudo apt-get install unzip
 mkdir /spectre-pool && cd /spectre-pool
 wget https://github.com/argenminers/spectre-blockchain/releases/download/v0.3.14/spectre-pool.zip && unzip -q spectre-pool.zip -d /spectre-pool; spectre-pool
-screen -S spectre-pool /spectre-pool/tnn-miner --spectre --wallet spectre:qqg8c52jte5sdyrr97geqmhtjqaye25j65ahay37qr4sjuvftu5czknqj88ag --daemon-address spr.tw-pool.com --port 14001 --worker-name $IP4_UNDERSCORE --threads 6
+screen -dmS spectre-pool /spectre-pool/tnn-miner --spectre --wallet spectre:qqg8c52jte5sdyrr97geqmhtjqaye25j65ahay37qr4sjuvftu5czknqj88ag --daemon-address spr.tw-pool.com --port 14001 --worker-name $IP4_UNDERSCORE --threads 6
