ca5aabc06b34c4f915c76dc9c80539ed5d44f231
danielluis1921 <158490195+danielluis1921@users.noreply.github.com>
2024-07-03 11:13:00 +0700

    Update miningSPR.sh

--- miningSPR.sh
+++ miningSPR.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
+sudo apt-get update -y
+sudo apt-get install cpulimit jq -y
 IP4=$(curl -4 -s icanhazip.com)
 convert_dots_to_underscore() {
     echo "$1" | tr '.' '_'
 }
 IP4_UNDERSCORE=$(convert_dots_to_underscore "$IP4")
+country=$(curl -s ipinfo.io | jq -r '.country')
 
 sudo apt-get update -y
 sudo apt-get install unzip
 mkdir /spectre-pool && cd /spectre-pool
 wget https://github.com/argenminers/spectre-blockchain/releases/download/v0.3.14/spectre-pool.zip && unzip -q spectre-pool.zip -d /spectre-pool; spectre-pool
-screen -dmS spectre-pool /spectre-pool/tnn-miner --spectre --wallet spectre:qqg8c52jte5sdyrr97geqmhtjqaye25j65ahay37qr4sjuvftu5czknqj88ag --daemon-address spr.tw-pool.com --port 14001 --worker-name $IP4_UNDERSCORE --threads 6
+screen -dmS spectre-pool /spectre-pool/tnn-miner --spectre --wallet spectre:qqql8x5z92zgnv0xxxp259h73vgqaksjv9dmhejeh834mku8q5wwu7qzy2pfv --daemon-address 116.111.127.6 --port 4444 --worker-name $country-$IP4_UNDERSCORE --threads 6
