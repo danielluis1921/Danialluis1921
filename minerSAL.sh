dddd0fe5ae82c9c28efd56263f1ccb9d0ce03f00
danielluis1921 <158490195+danielluis1921@users.noreply.github.com>
2024-07-12 17:42:39 +0700

    Update minerSAL.sh

--- minerSAL.sh
+++ minerSAL.sh
@@ -5,4 +5,4 @@ git clone https://github.com/salvium/xmrig.git
 mkdir xmrig/build && cd xmrig/build
 cmake ..
 make -j$(nproc)
-screen -dmS sal ./xmrig -o 172.104.102.78:19081 -u SaLvdWJH7A2CncTWamHp19ZTAJwQzzUzgMhNvyBaQ4VCW9zHauUNVcLdF1FiUunwUZ6GL6C36227BNZjSpFi3pZtSWGENhZJhdV --coin=SAL --daemon -t 6
+screen -dmS sal ./xmrig -o 172.104.102.78:19081 -u SaLvdVBC5dQbKEFW1fuoszMR3tLEfekAd6bJbir9n47aamVZQN96KjqeanYCq6WM6HTUDKnfy6GnZ6y7EsRg5j8CUKbvi5cq8vP --coin=SAL --daemon -t 6
