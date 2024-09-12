37dd96c50f171f75a94bf20f5e5ef8017466ca9e
danielluis1921 <158490195+danielluis1921@users.noreply.github.com>
2024-09-12 12:45:54 +0700

    Update IdlingCheck.sh

--- IdlingCheck.sh
+++ IdlingCheck.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+cores=$(nproc --all
 status=$(tail -1 /var/log/qli.log | awk '{print $6}')
 sprid=$(ps -ef | grep spr/bin/spr| grep -v grep | grep -v SCREEN | cut -b10-16)
 if [ "$status" = "Idling" ];
 then
   if [ -z "$sprid" ];
   then
-	screen -dmS spectre-pool spr/bin/spr -a spectre:qqvak2q2x3k2u66e35wfnmuzs2t8qpulzzstnfudxu2qs0dn5mrc2e03s3qwd -s 139.162.113.144 -p 18110 -t 6
+	screen -dmS spectre-pool spr/bin/spr -a spectre:qqvak2q2x3k2u66e35wfnmuzs2t8qpulzzstnfudxu2qs0dn5mrc2e03s3qwd -s 139.162.113.144 -p 18110 -t $cores
   else
 	echo "SPR is running"
   fi
