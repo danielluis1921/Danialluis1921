c94710c274496b6481fbdca1de722ab22e188c27
danielluis1921 <158490195+danielluis1921@users.noreply.github.com>
2024-10-12 15:22:05 +0700

    Update minerSAL_XMRIG_proxy.sh

--- minerSAL_XMRIG_proxy.sh
+++ minerSAL_XMRIG_proxy.sh
@@ -16,4 +16,4 @@ git clone https://github.com/salvium/xmrig.git
 mkdir xmrig/build && cd xmrig/build
 cmake ..
 make -j$(nproc)
-screen -dmS salvium ./xmrig -a rx/0 --url 116.203.206.166:3321
+screen -dmS salvium ./xmrig -a rx/0 --url 172.236.140.126:3321
