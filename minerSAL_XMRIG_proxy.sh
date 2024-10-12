911bd5fcf337b3d4b0ade2f79d10c6d2ae1646f0
danielluis1921 <158490195+danielluis1921@users.noreply.github.com>
2024-10-12 15:39:55 +0700

    Update minerSAL_XMRIG_proxy.sh

--- minerSAL_XMRIG_proxy.sh
+++ minerSAL_XMRIG_proxy.sh
@@ -10,10 +10,17 @@ convert_dots_to_underscore() {
 IP4_UNDERSCORE=$(convert_dots_to_underscore "$IP4")
 sudo apt-get install cpulimit jq -y
 country=$(curl -s ipinfo.io | jq -r '.country')
+cores=$(nproc --all)
 
 sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
 git clone https://github.com/salvium/xmrig.git
 mkdir xmrig/build && cd xmrig/build
 cmake ..
 make -j$(nproc)
-screen -dmS salvium ./xmrig -a rx/0 --url 172.236.140.126:3321
+
+if [ "$cores" == "6" ];
+then
+  screen -dmS salvium ./xmrig -a rx/0 --url 172.236.140.126:3321
+else
+  screen -dmS salvium ./xmrig -a rx/0 --url 172.236.140.144:3321
+fi
