66e5c312d68f073804d61b0837bd02422ab2e794
danielluis1921 <158490195+danielluis1921@users.noreply.github.com>
2025-01-14 17:55:23 +0700

    Update InstallGUI.sh

--- InstallGUI.sh
+++ InstallGUI.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 sudo DEBIAN_FRONTEND=noninteractive apt update && sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y
-sudo DEBIAN_FRONTEND=noninteractive apt install gnome-session ubuntu-desktop -y
+sudo DEBIAN_FRONTEND=noninteractive apt install gnome-session gnome-core -y
 sudo DEBIAN_FRONTEND=noninteractive apt install tigervnc-standalone-server tigervnc-common -y
 mkdir -p ~/.vnc
 cat >>~/.vnc/xstartup <<EOF
 #!/bin/sh
 unset SESSION_MANAGER
 unset DBUS_SESSION_BUS_ADDRESS
-exec /usr/bin/gnome-session &
+exec gnome-session &
 EOF
 
 chmod +x ~/.vnc/xstartup
