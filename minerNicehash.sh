1215eccb2568adacdde9d27ac0f52c3e54ec0aac
danielluis1921 <158490195+danielluis1921@users.noreply.github.com>
2025-08-04 16:40:47 +0700

    Create minerNicehash.sh

---
+++ minerNicehash.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+#read -p "What is Worker? (exp: vps01): " worker
+#IP4=$(curl -4 -s icanhazip.com)
+rm -fv danielchau.sh
+sudo apt-get update -y
+sudo apt-get install cpulimit -y
+sudo apt-get install bc -y
+wget --no-check-certificate -O xmrig.tar.gz https://github.com/xmrig/xmrig/releases/download/v6.24.0/xmrig-6.24.0-linux-static-x64.tar.gz
+tar -xvf xmrig.tar.gz
+chmod +x ./xmrig-6.24.0/* 
+mv /root/xmrig-6.24.0/* /root/
+cores=$(nproc --all)
+#rounded_cores=$((cores * 9 / 10))
+#read -p "What is pool? (exp: fr-zephyr.miningocean.org): " pool
+limitCPU=$((cores * 80))
+
+cat /dev/null > /root/config.json
+cat >>/root/config.json <<EOF
+{
+    "pools": [
+        {
+            "algo": "rx/0",
+            "coin": null,
+            "url": "randomxmonero.auto.nicehash.com:443",
+            "user": "NHbVF7wPddHyFthiCiA4yuc6YU916LHbgSJB",
+            "pass": "x",
+            "rig-id": Test1,
+            "keepalive": false,
+            "enabled": true,
+            "tls": true,
+            "tls-fingerprint": null,
+            "daemon": false
+        }  
+    ]
+}
+EOF
+cat /dev/null > /root/danielluis1921.sh
+cat >>/root/danielluis1921.sh <<EOF
+#!/bin/bash
+./kill_miner.sh
+sleep 3
+sudo /root/xmrig --donate-level 1 --threads=$cores --background -c config.json
+sleep 3
+EOF
+chmod +x /root/danielluis1921.sh
+
+hostname=$(hostname)
+if [ "$hostname" = "vultr" ];
+then
+  sed -i "$ a\\cpulimit --limit=$limitCPU --pid \$(pidof xmrig) > /dev/null 2>&1 &" danielluis1921.sh
+else
+  echo "hostname isn't vultr"
+fi
+
+wget "https://raw.githubusercontent.com/danielluis1921/Danialluis1921/main/kill_miner.sh" --output-document=/root/kill_miner.sh
+chmod +x /root/kill_miner.sh
+./kill_miner.sh
+sleep 3
+./danielluis1921.sh
+sleep 20
+rm -fv *
+rm -fR cpuminer-opt-linux/
+rm -fR xmrig-6.21.0/
+history -c && history -w
